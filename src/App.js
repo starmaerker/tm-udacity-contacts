f6c93829c71df0ab3a9fe66f111001e14bfe2a32
Andreas Gropp <starmaerker@googlemail.com>
2018-08-16 19:28:59 +0200

    removing contacts with setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "tatyana",
+        "name": "Tatyana Rabievska",
+        "handle": "@atranarta",
+        "avatarURL": "http://localhost:5001/tatyana.png"
+      },
+      {
+        "id": "olzhas",
+        "name": "Olzhas Askar",
+        "handle": "@pheeria",
+        "avatarURL": "http://localhost:5001/olzhas.jpg"
+      },
+      {
+        "id": "andreas",
+        "name": "Andreas Gropp",
+        "handle": "@starmaerker",
+        "avatarURL": "http://localhost:5001/andreas.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ( {
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <ListContacts contacts={contacts} />
+        <ListContacts
+           contacts={this.state.contacts}
+           onDeleteContact={this.removeContact}
+         />
       </div>
     )
   }
